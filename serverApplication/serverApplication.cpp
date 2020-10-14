ad34b58cc17ae70ccd7cadf43f3799650b36251f
capcha <arjen.robben2012@yandex.ru>
2020-10-14 22:56:58 +0700

    serverApp changed

--- serverApplication/serverApplication.cpp
+++ serverApplication/serverApplication.cpp
@@ -3,23 +3,85 @@
 #include <string>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+#include <list>
+#include <thread>
 
 using namespace std;
+
+struct client {
+	SOCKET socket = SOCKET_ERROR;
+	SOCKADDR_IN sockADDR_IN;
+	string name = "";
+
+	client() {};
+	client(SOCKET _socket, SOCKADDR_IN sADDR_IN, string _name) : socket(_socket), sockADDR_IN(sADDR_IN), name(_name) {};
+};
 
 // Необходимо, чтобы линковка происходила с DLL-библиотекой 
 // Для работы с сокетам
 #pragma comment(lib, "Ws2_32.lib")
 
-const char* SERVERIP = "127.0.0.1";
 const char* SERVERPORT = "2001";
 const int TEXTLEN = 1024;
 const int RECVTEXTLEN = 2048;
 const int PORTLEN = 6;
 const int IPLEN = 16;
+const int MAXUSERAMOUNT = 50;
+list<client> clients;
+
+
+int chat(client& clientI) {
+
+	char recvText[RECVTEXTLEN];
+	int retVal;
+
+	while (true) {
+		retVal = recv(clientI.socket, recvText, RECVTEXTLEN, 0);
+
+		if (retVal == SOCKET_ERROR) {
+			cerr << "bind failed with error: " << WSAGetLastError() << "\n";
+			closesocket(clientI.socket);
+			WSACleanup();
+			return 1;
+		}
+		
+		string result = clientI.name;
+
+
+		if (recvText == "exit") {
+			result.append(" left the chat");
+
+			closesocket(clientI.socket);
+
+			clients.remove(clientI);
+
+			return 1;
+		}
+
+
+		for (client i : clients) {
+			result = clientI.name;
+			result.append(": ");
+			result.append(recvText);
+			result.append("\n");
+
+			retVal = send(i.socket, result.c_str(), RECVTEXTLEN, 0);
+
+			if (retVal == SOCKET_ERROR) {
+				cerr << "send failed with error: " << WSAGetLastError() << "\n";
+				closesocket(i.socket);
+				clients.remove(i);
+				return 1;
+			}
+		}
+	}
+}
 
 int server() {
 	WSADATA wsaData;
-	char ip[IPLEN], port[PORTLEN];
+	char ip[IPLEN];
+	int userAmount = 0;
+	client tempClient;
 
 	int retVal = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -37,7 +99,7 @@ int server() {
 
 	// Инициализируем структуру, хранящую адрес сокета - addr.
 
-	retVal = getaddrinfo(SERVERIP, SERVERPORT, &hints, &addr);
+	retVal = getaddrinfo(INADDR_ANY, SERVERPORT, &hints, &addr);
 
 	// Если инициализация структуры адреса завершилась с ошибкой,
 	// выведем сообщением об этом и завершим выполнение программы 
@@ -96,10 +158,12 @@ int server() {
 		inet_ntop(hints.ai_family, &from.sin_addr, ip, INET_ADDRSTRLEN);
 
 		cout << "New connection accepted from " << ip << " port: " << htons(from.sin_port) << endl;
+		cout << "Users on server: " << ++userAmount << endl;
 
+		char clientName[RECVTEXTLEN];
 		char recvText[RECVTEXTLEN];
 
-		retVal = recv(clientSocket, recvText, TEXTLEN, 0);
+		retVal = recv(clientSocket, clientName, TEXTLEN, 0);
 
 		if (retVal == SOCKET_ERROR) {
 			cerr << "recv failed with error: " << WSAGetLastError() << "\n";
@@ -108,43 +172,45 @@ int server() {
 			return 1;
 		}
 
-		cout << "RECIEVED TEXT: " << recvText << endl;
+		if (userAmount <= MAXUSERAMOUNT) {
 
-		if (recvText == "stop") {
-			cout << "STOPPED";
-			break;
-		}
+			tempClient = client(clientSocket, from, clientName);
+			
+			char newConnection[RECVTEXTLEN] = "New client connected:\n";
+			strcat_s(newConnection, inet_ntoa(tempClient.sockADDR_IN.sin_addr));
+			strcat_s(newConnection, "\nHello, ");
+			strcat_s(newConnection, tempClient.name.c_str());
 
-		int j = 0;
-		string buf, result = "", recvTextStr = recvText;
- 
-		for (int i = 0; i < recvTextStr.length(); i++) {
-			if (recvText[i] == '.') {
-				buf = "";
-				buf = recvTextStr.substr(j, i - j);
-				result = result + buf + ". " + to_string(i - j + 1) + "\n";
-				j = i + 2;
+			for (client i : clients) {
+				retVal = send(i.socket, newConnection, RECVTEXTLEN, 0); 
+
+				if (retVal == SOCKET_ERROR) {
+					cerr << "send failed with error: " << WSAGetLastError() << "\n";
+					closesocket(i.socket);
+					clients.remove(i);
+					return 1;
+				}
 			}
+
+			clients.push_back(tempClient);
+
 		}
 
-		int len = recvTextStr.length();
-		buf = recvTextStr.substr(j, len);
-		result = result + buf + " " + to_string(len - j + 1) + "\n";
+		else {
+			--userAmount;
+			cout << "Server is full" << endl;
+			retVal = send(clientSocket, "Server is full", RECVTEXTLEN, 0);
 
-		retVal = send(clientSocket, result.c_str(), RECVTEXTLEN, 0);
+			if (retVal == SOCKET_ERROR) {
+				cerr << "send failed with error: " << WSAGetLastError() << "\n";
+				closesocket(clientSocket);
+				return 1;
+			}
 
-		if (retVal == SOCKET_ERROR) {
-			cerr << "send failed with error: " << WSAGetLastError() << "\n";
 			closesocket(clientSocket);
-			WSACleanup();
-			return 1;
 		}
 
-		cout << "MESSAGE IS SENT" << endl;
-
-		closesocket(clientSocket);
-
-		cout << "CLIENT SOCKET IS CLOSED" << endl;
+		thread threadI = thread(chat, tempClient.socket);
 	}
 
 	closesocket(serverSocket);
